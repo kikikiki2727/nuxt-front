589f940c9b3350a32c57a5ca02aec81bea2273ff
kikikiki2727 <kimori27271212@gmail.com>
2022-06-07 23:08:01 +0900

    sassインストール

--- components/homeComponent.vue
+++ components/homeComponent.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <div class="sample">
+    <p>aaa</p>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
+
+<style lang="scss">
+.sample {
+  > p {
+    color: red;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <p>レイアウト</p>
-    <!-- <Sample /> -->
     <slot />
   </div>
 </template>
